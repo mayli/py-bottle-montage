0d2cd21d7953618ec9afc379c0bd70f271243c41
Mengyang Li <mayli.he@gmail.com>
2022-03-21 19:42:54 -0700

    initial commit

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 from alpine:edge
-run apk add --no-cache py3-bottle py3-requests imagemagick
+run apk add --no-cache py3-pip imagemagick ttf-liberation && \
+        pip install --no-cache requests bottle eventlet
 expose 8080
 ADD main.py /
 CMD ["/main.py"]
-ENTRYPOINT ["python"]
+ENTRYPOINT ["python3"]
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+try:
+    import eventlet; eventlet.monkey_patch()
+except ImportError:
+    pass
+
 import tempfile
 import os
 import shutil
@@ -13,30 +18,42 @@ from bottle import post, get
 session = requests.Session()
 app = application = bottle.default_app()
 
-def download(urls, prefix):
-    names = []
-    for url in urls:
-        name = os.path.join(prefix, os.path.basename(url))
-        with open(name, 'wb') as f:
-            f.write(session.get(url).content)
-        names.append(name)
+def download(url, prefix):
+    name = os.path.join(prefix, os.path.basename(url))
+    with open(name, 'wb') as f:
+        resp = session.get(url)
+        resp.raise_for_status()
+        f.write(resp.content)
+    return name
+
+def download_func(arg):
+    return download(*arg)
+
+def download_urls(urls, prefix):
+    pool = eventlet.GreenPool()
+    names = list(pool.imap(download_func, [(url, prefix) for url in urls]))
     return names
 
 @get('/')
 def get_index():
-    return "montage api: " + open('/proc/loadavg').read()
+    with open('/proc/loadavg') as f:
+        return "montage api: " + f.read()
 
 @post('/montage')
 def montage_handler():
-    urls = request.json()
+    urls = request.json
+    print(' '.join(urls))
     dtemp = tempfile.mkdtemp()
-    names = download(urls, dtemp)
-    output = os.path.join(dtemp)
-    check_call(['montage'] + names + ['-frame', '5', '-geometry', '+1+1', output])
-    content = open(output, 'rb').read()
+    names = download_urls(urls, dtemp)
+    output = os.path.join(dtemp, 'frame.jpg')
+    check_call(['montage'] + names + ['-font', 'Liberation-Mono', '-frame', '5', '-geometry', '+1+1', output])
+    with open(output, 'rb') as f:
+        content = f.read()
     shutil.rmtree(dtemp)
     response.set_header('Content-type', 'image/jpeg')
     return content
 
 if __name__ == '__main__':
-    bottle.run(host = '0.0.0.0', port = 8080)
+    port = os.environ.get("PORT", 8080)
+    port = int(port)
+    bottle.run(server='eventlet', host = '0.0.0.0', port = port, debug=True)
